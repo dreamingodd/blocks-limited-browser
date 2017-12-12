4aa51327e19bcda424eba871837d761ee7356e16
dreamingodd <memorywilllast@gmail.com>
2017-12-12 20:43:20 +0800

    order-list almost done. lack of failure operation which depends on NJH

--- js/order-list.js
+++ js/order-list.js
@@ -3,6 +3,7 @@ $(function(){
     searchURL = searchURL.substring(1, searchURL.length);
     var searchText = searchURL.split("&")[0].split("=")[1];
     if (!searchText) searchText = ''
+    else $('#input-search-text').val(searchText)
 
     $.ajax({
         type:'get',
@@ -20,24 +21,89 @@ $(function(){
             }
         }
     })
+
+    $('body').on('click', '#search-btn', function(){
+        window.location.href = frontPath + "/invite/order-list.html?searchText=" + $('#input-search-text').val();
+    })
+
+    $('body').on('click', '.btn-confirmation', function(){
+        var id = $(this).parent().siblings('.order-id').html();
+        $.ajax({
+            type:'put',
+            url: backPath + "/invite/order/confirmation?id=" + id,
+            success: function(result){
+                var res = JSON.parse(result);
+                if (res.status != 200) {
+                    alert("对不起，出错了，请稍后再试！");
+                } else if(res.data){
+                    window.location.href = frontPath + "/invite/order-list.html?searchText=" + $('#input-search-text').val();
+                }
+            }
+        })
+    })
+
+    $('body').on('click', '.btn-failure', function(){
+    })
+
+    $('body').on('click', '.btn-delivery', function(){
+        var id = $(this).parent().siblings('.order-id').html();
+        $.ajax({
+            type:'put',
+            url: backPath + "/invite/order/delivery?id=" + id,
+            success: function(result){
+                var res = JSON.parse(result);
+                if (res.status != 200) {
+                    alert("对不起，出错了，请稍后再试！");
+                } else if(res.data){
+                    window.location.href = frontPath + "/invite/order-list.html?searchText=" + $('#input-search-text').val();
+                }
+            }
+        })
+    })
+
+    $('body').on('click', '.btn-finish', function(){
+        var id = $(this).parent().siblings('.order-id').html();
+        $.ajax({
+            type:'put',
+            url: backPath + "/invite/order/finish?id=" + id,
+            success: function(result){
+                var res = JSON.parse(result);
+                if (res.status != 200) {
+                    alert("对不起，出错了，请稍后再试！");
+                } else if(res.data){
+                    window.location.href = frontPath + "/invite/order-list.html?searchText=" + $('#input-search-text').val();
+                }
+            }
+        })
+    })
 })
 
 function convertOrderToTr(order) {
     var trStr = "";
 
     trStr += "<tr>"
-    trStr += "<td>" + order.id + "</td>"
+    trStr += "<td class='order-id'>" + order.id + "</td>"
     trStr += "<td>" + order.name + "</td>"
     trStr += "<td>" + order.cellphone + "</td>"
     trStr += "<td>" + order.address + "</td>"
-    trStr += "<td>" + order.quantity + "</td>"
     trStr += "<td>" + order.inviteCode + "</td>"
-    trStr += "<td>" + order.createdAt + "</td>"
+    trStr += "<td>" + order.createdAtStr + "</td>"
     trStr += "<td>" + order.comment + "</td>"
-    trStr += "<td>" + order.payChannel + "</td>"
+    payChannelText = ''
+    if (order.payChannel == 1) payChannelText = '支付宝'
+    if (order.payChannel == 2) payChannelText = '银行转账'
+    trStr += "<td>" + payChannelText + "</td>"
     trStr += "<td>" + order.payAccount + "</td>"
     trStr += "<td>" + order.payerName + "</td>"
+    trStr += "<td>" + order.quantity + "</td>"
     trStr += "<td>" + (order.sum/100) + "</td>"
+    statusText = ''
+    if (order.status == 0 || order.status == 1) statusText = '待审核'
+    if (order.status == 2) statusText = '审核通过'
+    if (order.status == 3) statusText = '审核未通过'
+    if (order.status == 4) statusText = '送货中'
+    if (order.status == 5) statusText = '订单完成'
+    trStr += "<td>" + statusText + "</td>"
     trStr += "<td>" + htmlOfButtons() + "</td>"
     trStr += "</tr>"
 
@@ -45,9 +111,9 @@ function convertOrderToTr(order) {
 }
 
 function htmlOfButtons() {
-    return '<button class="btn btn-primary">确认收款</button>&nbsp;'
-        + '<button class="btn btn-danger">确认失败</button>&nbsp;'
-        + '<button class="btn btn-warning">发货</button>&nbsp;'
-        + '<button class="btn">完成</button>&nbsp;'
+    return '<button class="btn btn-primary btn-confirmation">确认收款</button>&nbsp;'
+        + '<button class="btn btn-danger btn-failure">确认失败</button>&nbsp;'
+        + '<button class="btn btn-warning btn-delivery">发货</button>&nbsp;'
+        + '<button class="btn btn-default btn-finish">完成</button>&nbsp;'
     ;
 }
